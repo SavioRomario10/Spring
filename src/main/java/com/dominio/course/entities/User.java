217ad12f6fd48144928e6a786eb3dc3883fafe11
Sávio Romário <savioromario10@gmail.com>
2026-03-22 17:18:32 -0300

    increment H2 db

--- src/main/java/com/dominio/course/entities/User.java
+++ src/main/java/com/dominio/course/entities/User.java
@@ -1,15 +1,25 @@
 package com.dominio.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Column
   private String name;
+  @Column
   private String email;
+  @Column
   private String phone;
+  @Column
   private String password;
 
   public User() {}
