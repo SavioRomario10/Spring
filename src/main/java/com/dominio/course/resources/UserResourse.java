4f09ff78a97e1f17280c156be192a5b1fa21f02b
Sávio Romário <savioromario10@gmail.com>
2026-03-24 00:09:15 -0300

    User Update

--- src/main/java/com/dominio/course/resources/UserResourse.java
+++ src/main/java/com/dominio/course/resources/UserResourse.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -48,5 +50,11 @@ public class UserResourse {
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     service.delete(id);
     return ResponseEntity.noContent().build();
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+    obj = service.update(id, obj);
+    return ResponseEntity.ok().body(obj);
   }
 }
